4209e15e022f557b786188cf06c16c056855961f
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-06-21 17:26:11 +0900

    Initial Commit

---
+++ UiTableViewController Test 02/Controller/PlusViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PlusViewController.swift
+//  UiTableViewController Test 02
+//
+//  Created by D7702_10 on 2018. 6. 21..
+//  Copyright © 2018년 lse. All rights reserved.
+//
+
+import UIKit
+
+class PlusViewController: UIViewController {
+
+  
+    
+    
+    
+    @IBAction func funBtn(_ sender: Any) {
+    }
+    @IBAction func numBtn(_ sender: Any) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
